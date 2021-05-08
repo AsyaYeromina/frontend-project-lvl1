d6d68d6ad8c42f6071bfe3ef57afc97e31c6ebea
Asya Yeromina <asyayeromina@gmail.com>
2021-05-08 19:28:46 +0300

    step 4: adding eslint, fixing import/export mistks

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,4 @@
 #!/usr/bin/env node
-import { userName } from '../src/cli.js';
+import userName from '../src/cli.js';
+
+console.log(`Hello, ${userName}!`);
--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,7 @@
 import readlineSync from 'readline-sync';
+
 console.log('Welcome to the Brain Games!');
-// const readlineSync = require('readline-sync');
-export const userName = readlineSync.question('May I have your name? ');
-console.log('Hello, ' + userName + '!');
+
+const userName = readlineSync.question('May I have your name? ');
+
+export default userName;
