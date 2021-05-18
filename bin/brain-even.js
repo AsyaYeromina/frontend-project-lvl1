ffacf9da9d7ad2991e56e24a8a53411128b33610
Asya Yeromina <asyayeromina@gmail.com>
2021-05-18 14:34:19 +0300

    st5: adding counter to game

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,8 +1,8 @@
-// Brain-even.
 // Requests ansver 'yes' if the number is even, 'no' if the number is odd
 // Runs 3 times if user answer is correct. If no, starts from sсratch.
 
 import readlineSync from 'readline-sync';
+import userName from '../src/cli.js';
 
 console.log("Answer 'yes' if the number is even, otherwise answer 'no'.");
 
@@ -14,8 +14,6 @@ function isEven(number) {
   return !(number % 2);
 }
 
-const currentRandomNumber = generateRandomNumber();
-
 function convertAnswer(number) {
   if (isEven(number)) {
     return 'yes';
@@ -23,16 +21,29 @@ function convertAnswer(number) {
   return 'no';
 }
 
+let counter = 0;
+let currentRandomNumber = generateRandomNumber();
+const ATTEMPT_COUNTER = 3;
+
 function answerToUser(userAnswer) {
   if (userAnswer === convertAnswer(currentRandomNumber)) {
+    counter += 1;
+    if (counter === ATTEMPT_COUNTER) {
+      return `Congratulations, ${userName}!`;
+    }
     return 'Correct!';
   }
-  return `'${userAnswer}' is wrong answer ;(.
-Correct answer was '${convertAnswer(currentRandomNumber)}'.
-Let's try again, {userName}!`;
+  counter = ATTEMPT_COUNTER;
+  return `'${userAnswer}' is wrong answer ;( \nCorrect answer was '${convertAnswer(currentRandomNumber)}'. \nLet's try again, ${userName}!`;
 }
 
-console.log(`Question: ${currentRandomNumber}`);
-const userAnswer = readlineSync.question('Your answer: ');
+function init() {
+  while (counter < ATTEMPT_COUNTER) {
+    currentRandomNumber = generateRandomNumber();
+    console.log(`Question: ${currentRandomNumber}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    console.log(answerToUser(userAnswer));
+  }
+}
 
-console.log(answerToUser(userAnswer));
+init();
