6973b4bcb07acb66ba367c560a35d342f7253aa2
Asya Yeromina <asyayeromina@gmail.com>
2021-06-04 18:33:20 +0300

    St6: refactor and divide to modules brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,48 +1,5 @@
 // Requests answer 'yes' if the number is even, 'no' if the number is odd
-// Runs 3 times if user answer is correct. If no, starts from sсratch.
 
-import readlineSync from 'readline-sync';
-import userName from '../src/cli.js';
-import generateRandomNumber from '../modules/random-generator.js';
-// import ATTEMPT_COUNTER from '../modules/variables.js';
+import getRoundData from '../src/games/even.js';
 
-console.log('Answer \'yes\' if the number is even, otherwise answer \'no\'.');
-
-function isEven(number) {
-  return !(number % 2);
-}
-
-function convertAnswer(number) {
-  if (isEven(number)) {
-    return 'yes';
-  }
-  return 'no';
-}
-
-const ATTEMPT_COUNTER = 3;
-const LIMIT_RANDOM_NUMBER = 100;
-let counter = 0;
-let currentRandomNumber = generateRandomNumber(LIMIT_RANDOM_NUMBER);
-
-function answerToUser(userAnswer) {
-  if (userAnswer === convertAnswer(currentRandomNumber)) {
-    counter += 1;
-    if (counter === ATTEMPT_COUNTER) {
-      return `Congratulations, ${userName}!`;
-    }
-    return 'Correct!';
-  }
-  counter = ATTEMPT_COUNTER;
-  return `'${userAnswer}' is wrong answer ;( \nCorrect answer was '${convertAnswer(currentRandomNumber)}'. \nLet's try again, ${userName}!`;
-}
-
-function init() {
-  while (counter < ATTEMPT_COUNTER) {
-    currentRandomNumber = generateRandomNumber(LIMIT_RANDOM_NUMBER);
-    console.log(`Question: ${currentRandomNumber}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-    console.log(answerToUser(userAnswer));
-  }
-}
-
-init();
+getRoundData();
---
+++ src/games/even.js
@@ -0,0 +1,17 @@
+import gameEngine from '../index.js';
+import generateRandomNumber from '../../modules/random-generator.js';
+
+const GAME_RULES_MESSAGE = 'Answer \'yes\' if the number is even, otherwise answer \'no\'.';
+const LIMIT_RANDOM_NUMBER = 100;
+
+const isEven = (number) => !(number % 2);
+
+const convertAnswer = (number) => (isEven(number) ? 'yes' : 'no');
+
+function getRoundData() {
+  const curTask = generateRandomNumber(LIMIT_RANDOM_NUMBER);
+  const curSolution = convertAnswer(curTask);
+  return [curTask, curSolution];
+}
+
+export default () => gameEngine(GAME_RULES_MESSAGE, getRoundData);
