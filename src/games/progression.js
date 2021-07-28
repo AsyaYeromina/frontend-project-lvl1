2495c698fa411640f5a7fb1fa80f6f3451fd9e6c
Asya Yeromina <asyayeromina@gmail.com>
2021-07-28 16:51:51 +0300

    Fixed progression fun (for new random-gen. func)

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,17 +2,21 @@ import gameEngine from '../index.js';
 import generateRandomNumber from '../../modules/random-generator.js';
 
 const GAME_RULES_MESSAGE = 'What number is missing in the progression?';
+const MINIMAL_RANDOM_NUMBER = 0;
 const LIMIT_RANDOM_NUMBER = 100;
-const progressionLength = 10;
-const maxProgressionDifference = 20;
+const progrsMinLength = 5;
+const progrsMaxLength = 15;
+const minProgrsDifference = 1;
+const maxProgrsDifference = 20;
 
 function generateGameData() {
-  const firstRandomNumber = generateRandomNumber(LIMIT_RANDOM_NUMBER);
-  const progressionDifference = generateRandomNumber(maxProgressionDifference);
-  const randomArrayElementIndex = generateRandomNumber(progressionLength);
+  const firstRandomNumber = generateRandomNumber(MINIMAL_RANDOM_NUMBER, LIMIT_RANDOM_NUMBER);
+  const progrsDifference = generateRandomNumber(minProgrsDifference, maxProgrsDifference);
+  const progrsRandomLength = generateRandomNumber(progrsMinLength, progrsMaxLength);
+  const randomArrayElementIndex = generateRandomNumber(progrsMinLength, progrsRandomLength);
   const generatedArr = [firstRandomNumber];
-  for (let i = 0; i < progressionLength; i += 1) {
-    const nextNumber = (generatedArr[generatedArr.length - 1]) + progressionDifference;
+  for (let i = 0; i < progrsRandomLength; i += 1) {
+    const nextNumber = (generatedArr[generatedArr.length - 1]) + progrsDifference;
     generatedArr.push(nextNumber);
   }
   const correctSolution = generatedArr[randomArrayElementIndex];
