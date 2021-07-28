d1ee62d5bbf0169ad5f692f3797e0cacd5304cac
Asya Yeromina <asyayeromina@gmail.com>
2021-07-28 16:50:34 +0300

    Fixed random-generator function

--- modules/random-generator.js
+++ modules/random-generator.js
@@ -1,3 +1,4 @@
-const generateRandomNumber = (number) => Math.floor(Math.random() * number);
+// const generateRandomNumber = (number) => Math.floor(Math.random() * number);
+const generateRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 export default generateRandomNumber;
