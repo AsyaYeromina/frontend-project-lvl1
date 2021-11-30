573bf8003586587d4b8d89af5e19485a1e2e234d
Asya Yeromina <asyayeromina@gmail.com>
2021-11-30 13:14:23 +0200

    refact

--- src/games/even.js
+++ src/games/even.js
@@ -1,18 +1,18 @@
 import startGameEngine from '../index.js';
 import generateRandomNumber from '../../modules/random-generator.js';
 
-const GAME_RULES_MESSAGE = 'Answer \'yes\' if the number is even, otherwise answer \'no\'.';
+const EVEN_GAME_RULES_MESSAGE = 'Answer \'yes\' if the number is even, otherwise answer \'no\'.';
 const LIMIT_RANDOM_NUMBER = 100;
-const MINIMAL_RANDOM_NUMBER = 0;
+const MINIMAL_RANDOM_NUMBER = 1;
 
 const isEven = (number) => !(number % 2);
 
 const convertAnswer = (number) => (isEven(number) ? 'yes' : 'no');
 
-function generateGameData() {
-  const curTask = generateRandomNumber(MINIMAL_RANDOM_NUMBER, LIMIT_RANDOM_NUMBER);
-  const curSolution = convertAnswer(curTask);
-  return [curTask, curSolution];
+function generateEvenGameData() {
+  const evenGameTask = generateRandomNumber(MINIMAL_RANDOM_NUMBER, LIMIT_RANDOM_NUMBER);
+  const evenGameSolution = convertAnswer(evenGameTask);
+  return [evenGameTask, evenGameSolution];
 }
 
-export default () => startGameEngine(GAME_RULES_MESSAGE, generateGameData);
+export default () => startGameEngine(EVEN_GAME_RULES_MESSAGE, generateEvenGameData);
